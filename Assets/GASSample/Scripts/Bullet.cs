f5d5c82c869f7e011e07a6eb429d6fda8c9e7381
RainbowMin <123438952@qq.com>
2024-09-18 01:18:02 +0800

    补全Bullet和Enemy脚本的所有逻辑，prefab上都加了对应的脚本

--- Assets/GASSample/Scripts/Bullet.cs
+++ Assets/GASSample/Scripts/Bullet.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 using GAS;
+using GAS.Runtime;
 
 public class Bullet : MonoBehaviour
 {
     private Rigidbody2D _rb;
+    private AbilitySystemComponent _asc;
+    [SerializeField]private GameplayEffectAsset _geBulletDamageAsset;
+    private GameplayEffect _geBulletDamage;
+    
     
     private void Awake()
     {
         _rb = gameObject.GetComponent<Rigidbody2D>();
+        _asc = gameObject.GetComponent<AbilitySystemComponent>();
+        _geBulletDamage = new GameplayEffect(_geBulletDamageAsset);
     }
 
     public void Init(Vector2 position, Vector2 direction, float speed, float damage)
@@ -15,19 +22,25 @@ public class Bullet : MonoBehaviour
         // 设置出生点，速度
         transform.position = position;
         _rb.velocity = direction * speed;
+
+        _asc.InitWithPreset(1);
+        _asc.AttrSet<AS_Bullet>().InitAtk(damage);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        /*
         // 伤害生效
         if(other.gameObject.TryGetComponent(out AbilitySystemComponent enemy))
         {
+            if(enemy.HasTag(GTagLib.Faction_Enemy))
+            {
+                _asc.ApplyGameplayEffectTo(_geBulletDamage, enemy);
+                Destroy(gameObject);
+            }
         }
         else
         {
-            //Destroy(gameObject);
+            Destroy(gameObject);
         }
-        */
     }
 }
--- Assets/GASSample/Scripts/Enemy.cs
+++ Assets/GASSample/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using GAS.Runtime;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -5,21 +6,20 @@ public class Enemy : MonoBehaviour
     private const float BoomDistance = 2.5f;
     private Player _player;
     private Rigidbody2D _rb;
+    private AbilitySystemComponent _asc;
 
     private void Awake()
     {
         _rb = gameObject.GetComponent<Rigidbody2D>();
+        _asc = gameObject.GetComponent<AbilitySystemComponent>();
+
         GameRunner.Instance.RegisterEnemy(this);
     }
 
-    private void OnDestroy()
-    {
-        GameRunner.Instance.UnregisterEnemy(this);
-    }
-    
     private void Update()
     {
-        if (Chase()) Boom();
+        if (Chase()) 
+            Boom();
         
         if (_player != null)
         {
@@ -29,21 +29,59 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    private void OnDestroy()
+    {
+        GameRunner.Instance.UnregisterEnemy(this);
+        _asc.AttrSet<AS_Fight>().HP.UnregisterPostBaseValueChange(_OnHpChange);
+        _asc.AbilityContainer.AbilitySpecs()[GAbilityLib.Bomb.Name].UnregisterEndAbility(_OnBombEnd);
+        _asc.AbilityContainer.AbilitySpecs()[GAbilityLib.Die.Name].UnregisterEndAbility(_OnBombEnd);
+    }
+
     public void Init(Player player)
     {
         _player = player;
+
+        _asc.InitWithPreset(1);
+        _InitAttributes();
+
+        _asc.AbilityContainer.AbilitySpecs()[GAbilityLib.Bomb.Name].RegisterEndAbility(_OnBombEnd);
+        _asc.AbilityContainer.AbilitySpecs()[GAbilityLib.Die.Name].RegisterEndAbility(_OnBombEnd);
+    }
+
+    private void _InitAttributes()
+    {
+        _asc.AttrSet<AS_Fight>().InitHP(10);
+        _asc.AttrSet<AS_Fight>().InitSpeed(5);
+        _asc.AttrSet<AS_Fight>().InitAtk(20);
+
+        _asc.AttrSet<AS_Fight>().HP.RegisterPostBaseValueChange(_OnHpChange);
+    }
+
+    private void _OnBombEnd()
+    {
+        Destroy(gameObject);
+    }
+
+    private void _OnHpChange(AttributeBase attributeBase, float oldValue, float newValue)
+    {
+        if(newValue <= 0)
+        {
+            _Die();
+        }
     }
 
     private bool Chase()
     {
-        if (_player == null)
+        if (_player == null
+        || _asc.AttrSet<AS_Fight>().HP.CurrentValue <= 0
+        || _asc.HasTag(GTagLib.Event_Ban_Move))
         {
-            _rb.velocity =Vector2.zero;
+            _rb.velocity = Vector2.zero;
             return false;
         }
         
         var delta = (Vector2)(_player.transform.position - transform.position);
-        var speed = 5;
+        var speed = _asc.AttrSet<AS_Fight>().Speed.CurrentValue;
         _rb.velocity = delta.normalized * speed;
         
         var distance = delta.magnitude;
@@ -52,10 +90,13 @@ public class Enemy : MonoBehaviour
 
     private void Boom()
     {
+        _asc.TryActivateAbility(GAbilityLib.Bomb.Name);
+
     }
 
-    private void Die()
+    private void _Die()
     {
         GameRunner.Instance.AddScore();
+        _asc.TryActivateAbility(GAbilityLib.Die.Name);
     }
 }
